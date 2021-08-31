e5f7a4707e23b6af2f0b4ca56bd6249f101a2969
gasin <gasinsato@gmail.com>
2021-09-01 08:35:28 +0900

    metal

--- src/vec3.hpp
+++ src/vec3.hpp
@@ -53,6 +53,12 @@ public:
         return e[0]*e[0]+e[1]*e[1]+e[2]*e[2];
     }
 
+    bool near_zero() const {
+        // Return true if the vector is close to zero in all dimensions.
+        const auto s = 1e-8;
+        return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
+    }
+
     double e[3];
 };
 
@@ -110,6 +116,10 @@ inline vec3 random_in_unit_sphere() {
 
 vec3 random_unit_vector() {
     return unit_vector(random_in_unit_sphere());
+}
+
+vec3 reflect(const vec3& v, const vec3& n) {
+    return v - 2*dot(v,n)*n;
 }
 
 #endif
