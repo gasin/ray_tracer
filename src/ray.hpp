e97ec1fc22ade9fdcb4586887e849a0c1d769ecf
gasin <gasinsato@gmail.com>
2021-09-01 00:32:07 +0900

    ortho

--- src/ray.hpp
+++ src/ray.hpp
@@ -11,6 +11,10 @@ public:
     
     point3 origin()  const { return orig; }
     vec3 direction() const { return dir;}
+
+    point3 at(double t) const {
+        return orig + t*dir;
+    }
     
     point3 orig;
     vec3 dir;
